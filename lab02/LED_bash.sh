4a742868bc8f2e640f5151323b349de2ef269622
ffernandes <felipe@gocache.com.br>
2023-06-13 15:54:28 -0300

    Bash, Python, C and CPP programs to enable leds

---
+++ lab02/LED_bash.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+#----------------------------------------[ VARS ]
+ledRed=16
+ledYellow=20
+ledGreen=21
+#-----------------------------------------------|
+
+#-----------------------------------[ FUNCTIONS ]
+function setupLed() {
+    led=$1  # GPIO Number
+    mode=$2 # Mode: in / out
+
+    echo -en "Enable GPIO [ ${led} ] in mode [ ${mode} ]: "
+    echo $led >>"/sys/class/gpio/export"
+    sleep 1 # Wait 1 second to ensure export GPIO
+    echo $mode "/sys/class/gpio/gpio${led}/direction"
+    echo "Success"
+}
+
+function disableLed() {
+    led=$1 # GPIO Number
+
+    echo -en "Disable GPIO [ ${led} ]: "
+    echo $led >> "/sys/class/gpio/unexport"
+    sleep 1 # Wait 1 second to ensure unexport GPIO
+    echo "Success"
+}
+
+function setLedStatus() {
+    led=$1
+    status=$2
+
+    echo -en "Set GPIO [ ${led} ] value [ $status ]: "
+    echo $status >> "/sys/class/gpio/gpio${led}/value"
+    echo "Success"
+}
+
+#-----------------------------------------------|
+
+#----------------------------------------[ MAIN ]
+# Setup env
+setupLed ${ledRed} "out"
+setupLed ${ledYellow} "out"
+setupLed ${ledGreen} "out"
+
+for _ in $(seq 5); do
+
+    # Red LED 2 second
+    setLedStatus ${ledRed} 1
+    sleep 2
+    setLedStatus ${ledRed} 0
+
+    # Green LED 1 second
+    setLedStatus ${ledGreen} 1
+    sleep 1
+    setLedStatus ${ledGreen} 0
+
+    # Yellow LED 1 second
+    setLedStatus ${ledYellow} 1
+    sleep 1
+    setLedStatus ${ledYellow} 0
+
+done
+
+# Disable GPIO
+disableLed ${ledRed}
+disableLed ${ledYellow}
+disableLed ${ledGreen}
+#-----------------------------------------------|
---
+++ lab02/LED_c.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+//----------------------------------------[ VARS ]
+int leds[] = {16, 20, 21};
+int num_leds = sizeof(leds) / sizeof(leds[0]);
+
+const char *base_path = "/sys/class/gpio/";
+//-----------------------------------------------|
+
+//-----------------------------------[ FUNCTIONS ]
+void set_led_status(int gpio, int value) {
+    printf("Set GPIO [ %d ] value [ %d ]\n", gpio, value);
+
+    char path[128];
+    snprintf(path, sizeof(path), "%s/gpio%d/value", base_path, gpio);
+    FILE *f = fopen(path, "w");
+    fprintf(f, "%d", value);
+    fclose(f);
+}
+
+void setup_led(int gpio, const char *mode) {
+    printf("Enable GPIO [ %d ] in mode [ %s ]\n", gpio, mode);
+
+    FILE *f = fopen("/sys/class/gpio/export", "w");
+    fprintf(f, "%d", gpio);
+    fclose(f);
+
+    usleep(1000000);
+
+    char path[128];
+    snprintf(path, sizeof(path), "%s/gpio%d/direction", base_path, gpio);
+    f = fopen(path, "w");
+    fprintf(f, "%s", mode);
+    fclose(f);
+}
+
+void disable_led(int gpio) {
+    printf("Disable GPIO [ %d ]\n", gpio);
+
+    FILE *f = fopen("/sys/class/gpio/unexport", "w");
+    fprintf(f, "%d", gpio);
+    fclose(f);
+
+    usleep(1000000);
+}
+
+//-----------------------------------------------|
+
+//----------------------------------------[ MAIN ]
+int main() {
+    for (int i = 0; i < num_leds; i++)
+    {
+        setup_led(leds[i], "out");
+    }
+    
+
+    for (int i = 0; i < 5; i++) {
+
+        // Red LED 2 seconds
+        set_led_status(leds[0], 1);
+        sleep(2);
+        set_led_status(leds[0], 0);
+
+        // Green LED 1 second
+        set_led_status(leds[1], 1);
+        sleep(1);
+        set_led_status(leds[1], 0);
+
+        // Yellow LED 1 second
+        set_led_status(leds[2], 1);
+        sleep(1);
+        set_led_status(leds[2], 0);
+    }
+
+    for (int i = 0; i < num_leds; i++)
+    {
+        disable_led(leds[i]);
+    }
+
+    return 0;
+}
